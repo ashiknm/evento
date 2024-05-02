9c78b065732c5bd68bc7e8556e6c7b05bfb64183
ashik <ashikagumbe@gmail.com>
2024-05-02 08:39:42 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
  
   return (
     <main className="flex flex-col items-center pt-36 px-3">
-      <H1 >Find eents around you</H1>
+      <H1 >Find events around you</H1>
       <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
         Browse more than <span className="font-bold text-accent italic underline">10,000 events</span> around you
       </p>
